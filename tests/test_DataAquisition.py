28dde4f2b8e1f50536b0146a3fbc4c99e1d36f3c
Adam-Calleja <adam.calleja@student.manchester.ac.uk>
2024-01-31 01:02:46 +0000

    Create new unit test and pytest fixture
    Created a new pytest fixture 'temp_data_directory()' which creates a
    temporary directory to store mock data while unit tests are running.
    This directory is deleted after the unit test finishes.
    
    Created a new unit test case
    'test_get_parliamentary_constituencies_file_exists_correct_return()'
    which tests whether the function 'get_parliamentary_constituencies()'
    returns the correct list when the file
    'uk_parliamentary_constituencies.txt' exists.
    
    Renamed the directory 'data' to 'test_data' since I wanted to name the
    temporary directory 'data' and wanted to store the permanent mock data
    elsewhere.

--- tests/test_DataAquisition.py
+++ tests/test_DataAquisition.py
@@ -3,6 +3,9 @@ import sys
 sys.path.append('..')
 
 import pytest 
+import shutil
+from pathlib import Path
+
 import DataAquisition
 from typing import List
 
@@ -69,10 +72,26 @@ class TestDataAcquisition:
 
     """
 
+    @pytest.fixture
+    def temp_data_directory(request, tmp_path):
+        """
+        Creates a temporary 'data' directory to store mock data 
+        for unit tests. This directory will be deleted after each 
+        unit test. 
+        """
+        current_directory = Path.cwd()
+
+        data_directory = current_directory / "data"
+        data_directory.mkdir()
+
+        yield data_directory
+
+        shutil.rmtree(data_directory)
+
     def test_read_parliamentary_constituents_correct_return(self):
         """
-        Tests that the list returned by the function '
-        read_parliamentary_constituents()' is correct. 
+        Tests that the list returned by the function 
+        'read_parliamentary_constituents()' is correct. 
 
         This list should contain all of the parliamentary constituencies 
         given in the documentation for this test class. 
@@ -80,10 +99,29 @@ class TestDataAcquisition:
 
         # Arrange
 
-        # Assert
+        # Act
         parliamentary_constituent_list = DataAquisition.read_parliamentary_constituencies()
 
-        # Act 
+        # Assert
         assert parliamentary_constituent_list == EXPECTED_PARLIAMENTARY_CONSTITUENT_LIST, "read_parliamentary_constituents() did not return correct list"
-        
-    
+
+    def test_get_parliamentary_constituencies_file_exists_correct_return(self, temp_data_directory):
+        """
+        Tests that the list returned by the function 
+        'get_parliamentary_constituencies()' is correct when the file 
+        'uk_parliamentary_constituencies.txt' exists.
+
+        This list should contain all of the parliamentary constituencies 
+        given in the documentation for this test class.
+        """
+            
+        # Arrange
+        permanent_mock_data_file = Path.cwd() / "test_data" / "uk_parliamentary_constituents_test.txt"
+        temporary_mock_data_file = temp_data_directory / "uk_parliamentary_constituents.txt"
+        shutil.copy(permanent_mock_data_file, temporary_mock_data_file)
+
+        # Act
+        parliamentary_constituent_list = DataAquisition.get_parliamentary_constituencies()
+
+        # Assert
+        assert parliamentary_constituent_list == EXPECTED_PARLIAMENTARY_CONSTITUENT_LIST, "get_parliamentary_constituents() did not return correct list"
